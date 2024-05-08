83da12fdeeb9f969b7159694b3b1e516b63eaed1
desmond.nnebue <sirdesmond09@gmail.com>
2024-05-08 20:22:10 +0100

    fix

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def test_route():
     return {"message": "Pinged successfully!"}, 200
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(5000)
+    app.run(host='0.0.0.0', port=int(5000))
